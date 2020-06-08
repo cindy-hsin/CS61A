2573b509d886f2f1cf8d40c9d9cf96a046719e14
cindy-hsin <66403829+cindy-hsin@users.noreply.github.com>
2020-06-08 09:31:59 +0800

    Add Question Description

--- lab03-recursion v.s. iteration/is_prime.py
+++ lab03-recursion v.s. iteration/is_prime.py
@@ -1,3 +1,13 @@
+"""
+Q8: Is Prime
+Write a function is_prime that takes a single argument n and returns True if n is a prime number and False otherwise.
+Assume n > 1. We implemented this in Discussion 1 iteratively, now time to do it recursively!
+
+Hint: You will need a helper function! 
+Remember helper functions are useful if you need to keep track of more variables than the given parameters, 
+or if you need to change the value of the input.
+"""
+
 def is_prime(n):
     """Returns True if n is a prime number and False otherwise.
 
